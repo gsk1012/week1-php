629530cf59de207188aac7acee60841ec53ad13d
Gurpreet Singh <gsinghkaur1012@gmail.com>
2023-05-08 10:13:32 +0200

    Eerste commit

---
+++ opd-1.php
@@ -0,0 +1,24 @@
+<?php
+
+// vraag1
+$eersteGetal = 10;
+$tweedeGetal = 12;
+
+echo $eersteGetal + $tweedeGetal;
+
+// vraag2
+$eersteGetal = 10;
+$tweedeGetal = 12;
+echo $eersteGetal * $tweedeGetal;
+
+//vraag 3
+$mijnNaam = 'Gurpreet';
+echo "Welkom $mijnNaam";
+
+// vraag 4
+$mijnNaam = "Carl";
+$jouwNaam = "Camel";
+echo "$mijnNaam zit bij $jouwNaam in de klas!"
+
+
+?>
